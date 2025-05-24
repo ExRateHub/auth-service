93154dabe4adc29b0fcffe2fa38578ff846e612d
veenrok <veenrok@veenrok.com>
2025-05-24 18:31:44 +0300

    fix(migrations): Add alembic files.

---
+++ migrations/env.py
@@ -0,0 +1,78 @@
+from logging.config import dictConfig
+
+import yaml
+import asyncio
+
+from alembic import context
+from sqlalchemy.engine import Connection
+
+from src.core.config import LOGGING_CONFIG, get_settings
+from src.infrastructure.database.engine import create_engine_from_settings
+from src.infrastructure.database.models.base import metadata
+
+config = context.config
+settings = get_settings()
+
+
+with open(LOGGING_CONFIG) as logging_file:
+    logging_config = yaml.safe_load(logging_file)
+
+dictConfig(logging_config)
+
+target_metadata = metadata
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    context.configure(
+        url=settings.database.get_url(),
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations() -> None:
+    """In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+
+    engine = create_engine_from_settings(settings)
+    
+    async with engine.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await engine.dispose()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+
+    asyncio.run(run_async_migrations())
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
