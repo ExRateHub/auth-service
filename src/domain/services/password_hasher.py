f411690df597f9f13ecb865bda9096bf935705e3
Vlad Korneev <veenrok@veenrok.com>
2025-05-25 12:55:51 +0300

    feat(domain): Add password hasher protocol.

---
+++ src/domain/services/password_hasher.py
@@ -0,0 +1,10 @@
+from typing import Protocol
+
+
+class PasswordHasherProtocol(Protocol):
+    
+    def verify(self, raw_password: str, hashed_password: str) -> bool:
+        ...
+
+    def hash(self, raw_password: str) -> str:
+        ...
