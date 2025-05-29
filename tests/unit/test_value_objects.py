a67a5728f2e4ce96759e99d136f2ddd1e19407be
veenrok <veenrok@veenrok.com>
2025-05-29 18:25:49 +0300

    style: clean up test_value_objects.py formatting and parametrize syntax

--- tests/unit/test_value_objects.py
+++ tests/unit/test_value_objects.py
@@ -6,24 +6,25 @@ from domain.value_objects.hashed_password import HashedPassword
 
 
 class TestEmail:
-
     @pytest.mark.parametrize(
         "email", ["example@domain.com", "example.example@domain.com", "example+example@domain.com"]
     )
     def test_valid_emails(self, email: str) -> None:
         assert email == Email(email).as_generic_type()
 
-    @pytest.mark.parametrize(
-        "email", ["example@domain", "@domain.com", "example@", "example"]
-    )
+    @pytest.mark.parametrize("email", ["example@domain", "@domain.com", "example@", "example"])
     def test_invalid_emails(self, email: str) -> None:
         with pytest.raises(InvalidEmail):
             Email(email)
 
-class TestHashedPassword:
 
+class TestHashedPassword:
     @pytest.mark.parametrize(
-        "hashed_password", ["$<algorithm>$<options>$<salt>$<hash>", "$<algorithm>$<options-1>$<options-2>$<salt>$<hash>",]
+        "hashed_password",
+        [
+            "$<algorithm>$<options>$<salt>$<hash>",
+            "$<algorithm>$<options-1>$<options-2>$<salt>$<hash>",
+        ],
     )
     def test_valid_mfc(self, hashed_password: str) -> None:
         _, algorithm, *options, salt, password_hash = hashed_password.split("$")
@@ -34,9 +35,7 @@ class TestHashedPassword:
         assert salt == HashedPassword(hashed_password).salt
         assert password_hash == HashedPassword(hashed_password).hash
 
-    @pytest.mark.parametrize(
-        "hashed_password", ["$<algorithm>$<options>$<salt>"]
-    )
+    @pytest.mark.parametrize("hashed_password", ["$<algorithm>$<options>$<salt>"])
     def test_invalid_mfc(self, hashed_password: str) -> None:
         with pytest.raises(InvalidHashedPassword):
             HashedPassword(hashed_password)
