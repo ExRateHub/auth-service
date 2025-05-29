0936810ff2b66b912597ae611174794e63e350d5
Vlad Korneev <veenrok@veenrok.com>
2025-05-29 18:20:57 +0300

    fix: Update BaseEntity fields to be keyword-only and initialize updated_at in __post_init__

--- src/domain/entities/base.py
+++ src/domain/entities/base.py
@@ -5,11 +5,15 @@ from dataclasses import dataclass, field
 from typing import Any, Self
 
 
-@dataclass()
+@dataclass
 class BaseEntity(ABC):
-    id: uuid.UUID = field(default_factory=uuid.uuid4)
-    created_at: datetime.datetime = field(default_factory=lambda: datetime.datetime.now(datetime.UTC))
-    updated_at: datetime.datetime = field(default_factory=lambda: datetime.datetime.now(datetime.UTC))
+    id: uuid.UUID = field(default_factory=uuid.uuid4, kw_only=True)
+    created_at: datetime.datetime = field(default_factory=lambda: datetime.datetime.now(datetime.UTC), kw_only=True)
+    updated_at: datetime.datetime | None = field(default=None, kw_only=True)
+
+    def __post_init__(self) -> None:
+        if self.updated_at is None:
+            self.updated_at = self.created_at
 
     @classmethod
     @abstractmethod
@@ -23,11 +27,3 @@ class BaseEntity(ABC):
     def _update_timestamp(self) -> None:
         """(protected) update the `updated_at` timestamp to now."""
         self.updated_at = datetime.datetime.now(datetime.UTC)
-
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, BaseEntity):
-            return False
-        return type(self) is type(other) and self.id == other.id
-
-    def __hash__(self) -> int:
-        return hash((type(self), self.id))
