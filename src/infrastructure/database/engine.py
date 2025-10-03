0aad9fb3dd921f397af4e80cf14e561096a6f299
Vlad Korneev <veenrok@veenrok.com>
2025-10-04 02:49:27 +0300

    refactor: Rename db engine helper create_engine_from_settings to get_async_engine_from_settings

--- src/infrastructure/database/engine.py
+++ src/infrastructure/database/engine.py
@@ -1,10 +1,11 @@
 import sqlalchemy as sa
+from sqlalchemy import create_engine, Engine
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
 from core.config import Settings
 
 
-def create_engine_from_settings(settings: Settings) -> AsyncEngine:
+def get_async_engine_from_settings(settings: Settings) -> AsyncEngine:
     engine = create_async_engine(
         url=settings.database.get_url(),
         poolclass=sa.NullPool,
