ed278c35fd381788f3256ac7efd44e0b51daf124
Vlad Korneev <veenrok@veenrok.com>
2025-10-04 02:50:03 +0300

    feat: Remove deprecated mapper

--- src/infrastructure/database/mapper.py
+++
@@ -1,9 +0,0 @@
-from adaptix import Retort, name_mapping
-
-from infrastructure.database.models import UserModel
-
-
-def get_mapper() -> Retort:
-    mapper = Retort()
-    mapper.extend(recipe=[name_mapping(UserModel, map={"email": ["email", "value"]})])
-    return mapper
