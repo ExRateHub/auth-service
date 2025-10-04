cd49213d306c410d5c7dbbf373c4b70a573038b4
Vlad Korneev <veenrok@veenrok.com>
2025-10-04 16:02:54 +0300

    feat: Add handler exception InvalidCredentialsError.

--- src/interface/http/exception_handlers.py
+++ src/interface/http/exception_handlers.py
@@ -3,7 +3,7 @@ import typing
 from litestar import Request, Response, status_codes
 
 from application.errors import ApplicationError
-from domain.errors import DomainError, ValidationError
+from domain.errors import DomainError, ValidationError, SecurityError, InvalidCredentialsError
 
 
 def get_exception_handlers_map() -> dict[type[Exception], typing.Callable[[Exception], Response[dict]]]:
@@ -23,6 +23,15 @@ def domain_error_handler(request: Request, error: DomainError) -> Response[dict]
         return Response(
             content=content,
             status_code=status_codes.HTTP_400_BAD_REQUEST,
+        )
+    elif isinstance(error, InvalidCredentialsError):
+        content = {
+            "status_code": status_codes.HTTP_401_UNAUTHORIZED,
+            "details": "Invalid login or password",
+        }
+        return Response(
+            content=content,
+            status_code=status_codes.HTTP_401_UNAUTHORIZED,
         )
     raise RuntimeError
 
