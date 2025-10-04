bf1b602a6b34a043b0bc45469d46410c12eaafc2
Vlad Korneev <veenrok@veenrok.com>
2025-10-04 10:55:39 +0300

    feat: Add setup structlog

--- src/interface/http/asgi.py
+++ src/interface/http/asgi.py
@@ -1,8 +1,11 @@
 from dishka import make_async_container
 from dishka.integrations.litestar import setup_dishka
 from litestar import Litestar
+from litestar.logging import StructLoggingConfig
+from litestar.openapi import OpenAPIConfig
 
 from core.di import AppProvider
+from core.logging import get_logger, setup_logging
 from interface.http.controlles.system import health
 
 from interface.http.controlles.auth import AuthController
@@ -19,10 +22,12 @@ def create_asgi_application() -> Litestar:
             AuthController,
             health,
         ],
-        openapi_config=OpenAPIConfig(title="ExRateHub API", version=1),
+        openapi_config=OpenAPIConfig(title="ExRateHub API", version="1.0.0dev"),
+        logging_config=StructLoggingConfig()
     )
 
     container = make_async_container(AppProvider())
 
     setup_dishka(container=container, app=app)
+    setup_logging()
     return app
