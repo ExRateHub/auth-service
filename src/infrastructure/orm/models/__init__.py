c82c9f248f31faac771178ddac11ca456c4a7373
Vlad Korneev <veenrok@veenrok.com>
2025-10-04 17:12:13 +0300

    feat: Register auth token model.

--- src/infrastructure/orm/models/__init__.py
+++ src/infrastructure/orm/models/__init__.py
@@ -1,8 +1,10 @@
 from .base import BaseModel, TimestampedModel
+from .auth_token import AuthTokenModel
 from .user import UserModel
 
 __all__ = (
     "BaseModel",
     "TimestampedModel",
+    "AuthTokenModel",
     "UserModel",
 )
