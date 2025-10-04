8949ccead88092d80880a342ebd586221cb4f598
Vlad Korneev <veenrok@veenrok.com>
2025-10-04 16:27:15 +0300

    feat: Add login endpoint.

--- src/interface/http/controlles/auth.py
+++ src/interface/http/controlles/auth.py
@@ -3,9 +3,11 @@ from dishka.integrations.litestar import inject
 from litestar import post
 from litestar.controller import Controller
 
+from application.use_cases.login_user import LoginUserCommand, LoginUserUseCase
 from application.use_cases.register_user import RegisterUserUseCase, RegisterUserCommand
 from domain.value_objects.username import Username
-from interface.http.schemas.auth import UserRegistrationSchema, UserSchema
+from interface.http.schemas.auth import UserRegistrationSchema, UserSchema, UserLoginSchema, LoginResponseSchema, \
+    TokenSchema
 
 
 class AuthController(Controller):
@@ -26,3 +28,27 @@ class AuthController(Controller):
         await register_user_use_case.execute(command=command)
 
         return UserSchema(username=data.username)
+
+    @post("/login")
+    @inject
+    async def login(
+        self,
+            data: UserLoginSchema,
+            login_user_use_case: FromDishka[LoginUserUseCase]
+
+    ) -> LoginResponseSchema:
+        """
+        User login endpoint.
+        :param data: Payload data.
+        :type data: UserLoginSchema
+        :return: Public user schema.
+        """
+        command = LoginUserCommand(login=Username(data.login), password=data.password)
+
+        user, auth_token, token_key = await login_user_use_case.execute(command=command)
+
+        response = LoginResponseSchema(
+            user=UserSchema(username=user.username.as_generic_type()),
+            token_key=TokenSchema(key=token_key.as_generic_type())
+        )
+        return response
