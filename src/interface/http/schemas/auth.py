e10de1d44e60d99104a1467fcff166ceed6e8ec4
Vlad Korneev <veenrok@veenrok.com>
2025-10-04 16:25:54 +0300

    feat: Add user login and login response schemas.

--- src/interface/http/schemas/auth.py
+++ src/interface/http/schemas/auth.py
@@ -7,6 +7,22 @@ class UserRegistrationSchema(BaseModel):
     username: str
     password: str
 
+class UserLoginSchema(BaseModel):
+    """User login schema"""
+    login: str
+    password: str
+
+
+
 class UserSchema(BaseModel):
     """User schema"""
     username: str
+
+class TokenSchema(BaseModel):
+    """User schema"""
+    key: str
+
+class LoginResponseSchema(BaseModel):
+    """Login response schema"""
+    user: UserSchema
+    token_key: TokenSchema
